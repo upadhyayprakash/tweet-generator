534b7ee75f930623b5c3ebb4a64f06a20b16886e
upadhyayprakash <prakash.upadhyay93@yahoo.com>
2022-03-24 17:51:55 +0000

    Deploying to gh-pages from @ upadhyayprakash/tweet-generator@86a6dc2cfa9334d151d9c067b3ecdadfb807af3f 🚀

---
+++ _next/static/sHiDqeAqO8PFGABPa6DuL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/479-d400f4f15d24c7e0.js","static/css/c86cca7deffd535e.css","static/chunks/pages/index-c872d78e22938e3c.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
