480f1e94463f8a098e43d26499c62065007bf02e
Prakash Upadhyay <prakash.upadhyay93@yahoo.com>
2022-03-24 18:43:38 +0530

    config changed

--- next.config.js
+++ next.config.js
@@ -3,12 +3,16 @@ const withReactSvg = require("next-react-svg");
 const path = require("path");
 const isProd = (process.env.NODE_ENV || "production") === "production";
 
-module.exports = withReactSvg({
-  include: path.resolve(__dirname, "public/icons"),
-  compiler: {
-    // ssr and displayName are configured by default
-    styledComponents: true,
-  },
+module.exports = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : "",
-});
+};
+// withReactSvg({
+//   include: path.resolve(__dirname, "public/icons"),
+//   compiler: {
+//     // ssr and displayName are configured by default
+//     styledComponents: true,
+//   },
+//   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+//   assetPrefix: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : "",
+// });
