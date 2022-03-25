c6d3ecf236b0e758cc45fa4ec96c05da23ee21a8
upadhyayprakash <prakash.upadhyay93@yahoo.com>
2022-03-25 12:31:58 +0000

    Deploying to gh-pages from @ upadhyayprakash/tweet-generator@92e6821bd137cbc10cabdb8c09e8d80193a1a9c2 🚀

--- _next/static/jehvwIuyE_EDl8DF6FbER/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/994-fe9f1767c247ad7f.js","static/css/c86cca7deffd535e.css","static/chunks/pages/index-c556d37e63ee06da.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/o7902uFn7HYd-84YAVqdR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/994-fe9f1767c247ad7f.js","static/css/c86cca7deffd535e.css","static/chunks/pages/index-3a89e54343772139.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
